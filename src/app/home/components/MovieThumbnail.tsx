f5157f30383ccafeb734573580dece043798fbb5
William Liu <wye139629@gmail.com>
2023-07-30 10:21:07 +0800

    feat: add default image and error image

--- src/app/home/components/MovieThumbnail.tsx
+++ src/app/home/components/MovieThumbnail.tsx
@@ -1,19 +1,51 @@
+"use client";
+
 import Image from "next/image";
 import config from "@/config";
+import { useState } from "react";
+
+function shimmer(w: number, h: number) {
+  return `<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+}
+
+function toBase64(str: string) {
+  return typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+}
 
 export default function MovieThumbnail({
   backgroundPath,
 }: {
   backgroundPath: string;
 }) {
+  const [isError, setIsError] = useState(false);
   return (
     <div className="relative h-32 min-w-[260px] cursor-pointer">
       <Image
         className="rounded-sm md:rounded object-cover"
-        src={`${config.imageEndpoint}/w500${backgroundPath}`}
+        src={
+          !isError
+            ? `${config.imageEndpoint}/w500${backgroundPath}`
+            : "/assets/image-error.jpeg"
+        }
         alt="movie-img"
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        placeholder="blur"
+        blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+        onError={() => setIsError(true)}
       />
     </div>
   );
